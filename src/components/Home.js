09ae3bd0704526dc86321b536276550229d56fe6
Julia <ottenbreitjulia06@gmail.com>
2025-11-01 16:00:57 -0700

    added colour to the navigation bar

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>I'm Julia</h1>
+            <h2>Welcome to my website</h2>
+            <p>
+                yadayyadaydaada
+            </p>
+        </div>
+    );
+};
+
+export default Home; 
